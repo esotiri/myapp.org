831f338e3b2196dcac00c38103884997913d9fe3
esotiri <esotiri@hmdc.harvard.edu>
2014-11-30 18:25:19 -0500

    Update stage1.sh

--- deploy/setup/stage1.sh
+++ deploy/setup/stage1.sh
@@ -3,52 +3,43 @@
 #su $SUDO_USER -s /bin/sh -c '. s1.sh'
 # user
 useradd plaid
-#plaid ALL=(root) NOPASSWD: /sbin/service httpd restart
+# EPEL already enabled on HMDC VM
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.noarch.rpm
 # base                                                                                                                
-yum install -y  wget
+# yum install -y  wget
 # install postgres server 9.3  ##did not work with postgres##
 #wget http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
 #rpm -ivh pgdg-centos93-9.3-1.noarch.rpm
 #yum install python27-python-psycopg2.x86_64
 
-# Epel
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.noarch.rpm
-
 echo "install apache ..."
 yum install -y httpd mod_wsgi ack elinks libjpeg-turbo-devel
-
 echo "softare collection"
 rpm --import http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/os/RPM-GPG-KEY-sl
 yum install -y http://ftp.scientificlinux.org/linux/scientific/6.4/x86_64/external_products/softwarecollections/yum-conf-softwarecollections-1.0-1.el6.noarch.rpm
-
 echo "Install Python27"
 yum install -y python27
-
 echo "Setting up Django app" ##django1.7 => python2.7; django1.6=> python2.6
 echo "instll pip"
 scl enable python27 "easy_install pip"
-
 echo "Install virtualenvwrapper"
 scl enable python27 "pip install virtualenvwrapper"
-
+#
 echo "Setup virtualenv directory"
 mkdir -p /webapps/virtualenvs
-chown plaid /webapps/virtualenvs
-mkdir /webapps/code
-chown plaid /webapps/code
-
-# 
+24	chown plaid /webapps/virtualenvs
+25	mkdir /webapps/code
+26	chown plaid /webapps/code
+3
 su plaid -l -s /bin/sh -c 'cd /webapps/code && cp -r /git/myapp.org .'
 cp /webapps/code/myapp.org/deploy/files/etc/sudoers.d/plaid /etc/sudoers.d
 chmod 640 /etc/sudoers.d/plaid
-
+#
 cp /git/myapp.org/myapp_org/myapp_org/settings/secret_settings_prod.json /webapps/code/myapp.org/myapp_org/myapp_org/settings/secret_settings_prod.json
 chown plaid:apache /webapps/code/myapp.org/myapp_org/myapp_org/settings/secret_settings_prod.json
 chmod 440 /webapps/code/myapp.org/myapp_org/myapp_org/settings/secret_settings_prod.json
-
 #
 # Create directory for sqlite db
-#
 echo "Create general data directory"
 #
 mkdir -p /webapps/data/myapp_org
@@ -77,7 +68,10 @@ chown plaid /var/www/myapp_org
 # echo "Create data directory for uploaded files"
 mkdir -p /webapps/data/myapp_org/myapp_org_uploaded_files
 chown apache /webapps/data/myapp_org/myapp_org_uploaded_files
-
+#
+# run main setup script as "plaid" user with python 2.7
+#
+su plaid -l -s /bin/sh -c 'scl enable python27 "/webapps/code/myapp.org/deploy/setup/stage2.sh"'
 # Permissions for database
 #
 chown plaid:apache /webapps/data/myapp_org/sqlite/myapp_org.db3
@@ -88,6 +82,4 @@ chkconfig httpd on
 # on HMDC VM, changed SELinux to "permissive" in /etc/selinux/config
 sed -i -e 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
 
-# run main setup script as "plaid" user with python 2.7
-#
-su plaid -l -s /bin/sh -c 'scl enable python27 "/webapps/code/myapp.org/deploy/setup/stage2.sh"'
+
